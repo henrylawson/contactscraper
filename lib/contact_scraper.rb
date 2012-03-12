11c01a873be4f716cbbe963ca02d1b0fe9828359
Henry Lawson <henry.lawson@foinq.com>
2012-03-13 00:15:39 +0530

    Extracted out some constants to make the code more readable

--- lib/contact_scraper.rb
+++ lib/contact_scraper.rb
@@ -3,11 +3,19 @@ require 'selenium-webdriver'
 
 class ContactScraper
 
+  CONTACT_SPAN_CLASS = "jCVCl"
+  NEXT_PAGE_LOADED_TEXT_INDICATOR_ID = ":68"
+  NEXT_BUTTON_ID = ":65"
+  LONG_TIMEOUT = 10
+  SHORT_TIMEOUT = 5
+
   def initialize
     @driver = Selenium::WebDriver.for :firefox
     wait_for_user_to_login
     navigate_to_contacts_section
-    wait_for :element => {:class => "jCVCl"}, :until_timeout => 10
+    wait_for :element => {:class => CONTACT_SPAN_CLASS}, :until_timeout => LONG_TIMEOUT
+    wait_for :element => {:id => NEXT_PAGE_LOADED_TEXT_INDICATOR_ID}, :until_timeout => LONG_TIMEOUT
+    click_next_link
     puts ContactParser.new(raw_html).contacts
   end
 
@@ -17,8 +25,20 @@ class ContactScraper
     end
   end
 
+  def click_next_link
+    initial_value = next_page_loaded_indicator_element
+    element = @driver.find_element(:id, NEXT_BUTTON_ID)
+    element.click
+    wait = Selenium::WebDriver::Wait.new(:timeout => SHORT_TIMEOUT)
+    wait.until { initial_value != next_page_loaded_indicator_element }
+  end
+
+  def next_page_loaded_indicator_element
+    @driver.find_element(:id, NEXT_PAGE_LOADED_TEXT_INDICATOR_ID).text
+  end
+
   def wait_for settings
-    wait = Selenium::WebDriver::Wait.new(:timeout => settings[:until_timeout]) # seconds
+    wait = Selenium::WebDriver::Wait.new(:timeout => settings[:until_timeout])
     wait.until { @driver.find_element(settings[:element]) }
   end
 
